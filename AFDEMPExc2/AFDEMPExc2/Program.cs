751757af8d6e3d3fd5010d6b58bd3ed3d4c21535
troumel <62116356+troumel@users.noreply.github.com>
2020-03-12 21:26:46 +0200

    Add files via upload

---
+++ AFDEMPExc2/AFDEMPExc2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFDEMPExc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2)
+                Console.WriteLine(num1);
+            else
+                Console.WriteLine(num2);
+        }
+
+
+    }
+}
