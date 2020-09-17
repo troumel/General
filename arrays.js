0aaf93048935ef2a01404146afa82ed69f70ef19
troumel <62116356+troumel@users.noreply.github.com>
2020-09-17 16:17:04 +0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,92 @@
+
+// let topSongs = [
+//   'harvester of sorrow',
+//   'dyers eve',
+//   'down with the sickness'
+// ];
+
+// topSongs.push('nothing else matters', 'zombie');
+// // topSongs.pop();
+// topSongs.unshift('say just words', 'Lost');
+
+// console.log(topSongs);
+
+// var fruits, text;
+
+// fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// console.log(topSongs.concat(fruits));
+// let indexOfBanana = fruits.indexOf("Apple");
+// if (indexOfBanana != -1) {
+//   fruits[indexOfBanana] = "Karpouzi";
+// }
+
+// text = "<ul>";
+// console.log(text);
+// fruits.forEach(myFunction);
+// console.log(text);
+// text += "</ul>";
+// console.log(text);
+// document.getElementById("demo").innerHTML = text;
+
+// function myFunction(value) {
+//   text += "<li>" + value + "</li>";
+// }
+
+
+// console.log(fruits);
+
+const fruits = ['banana', 'karpouzi'];
+const fruits2 = ['mango', 'peopni'];
+
+// console.log(fruits.concat(fruits2)); 
+
+let arrAsAString = fruits.join(' & ');
+// console.log(arrAsAString);
+// console.log(fruits);
+
+const animalArr = [
+    ['frida', 'Judi', 'Barry'],
+    ['dogydog', 'elephant', 'bat']
+];
+animalArr[1][0] = 'dogaros';
+
+// console.log(animalArr[1][0]);
+
+// console.log('--------------------');
+
+// const spaceship = {
+//     passengers:  [{name: 'teo'}],
+//     telescope: {
+//        yearBuilt: 2018,
+//        model: '91031-XLT',
+//        focalLength: 2032 
+//     },
+//    crew: {
+//        captain: { 
+//            name: 'Sandra', 
+//            degree: 'Computer Engineering', 
+//            encourageTeam() { console.log('We got this!') } 
+//         }
+//    },
+//    engine: {
+//        model: 'Nimbus2000'
+//     },
+//     nanoelectronics: {
+//         computer: {
+//            terabytes: 100,
+//            monitors: 'HD'
+//         },
+//        backup: {
+//           battery: 'Lithium',
+//           terabytes: 50
+//         }
+//    }
+// }; 
+
+// console.log(spaceship.nanoelectronics.backup.battery);
+// let firstPassenger = spaceship.passengers[0];
+// console.log(firstPassenger);
+
+
+
