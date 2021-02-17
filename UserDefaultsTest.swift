f11af9e51594666e6edaf71d3db2c4075c8a60f0
troumel <62116356+troumel@users.noreply.github.com>
2021-02-17 18:20:18 +0200

    Add files via upload

---
+++ UserDefaultsTest.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+
+    var body: some View {
+        
+        Button("Tap count: \(tapCount)") {
+            tapCount += 1
+            UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
