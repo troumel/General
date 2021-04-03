626b81edf66a4f4ac4a2310438bce7b26a7148de
troumel <62116356+troumel@users.noreply.github.com>
2021-04-03 23:40:43 +0300

    Add files via upload

---
+++ nearestToTen.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+"""
+    Write a program to check if a given string begins with "fix". "f" can be any character
+    or number, also.
+"""
+
+func ex13(word: String) -> Bool {
+    
+    var result = word
+    
+    result.removeFirst()
+    
+    if result.hasPrefix("ix") {
+        return true
+    }
+    
+    return false
+}
+
+print(ex13(word: "six dogsa")) // prints true
+
