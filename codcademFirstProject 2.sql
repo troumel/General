0597ba8e54c95060579962f7e312859e42b8e73a
troumel <62116356+troumel@users.noreply.github.com>
2021-04-10 21:13:54 +0300

    Add files via upload

---
+++ codcademFirstProject 2.sql
@@ -0,0 +1,31 @@
+--CREATE TABLE friends
+--(id INTEGER, name VARCHAR(50), birthday DATE);
+SELECT * FROM friends;
+
+--INSERT INTO friends
+--VALUES
+--(2, 'Jane Mary', '1993-03-20'),
+--(3, 'Mark Selby', '1980-05-12');
+
+--ALTER TABLE friends
+--ADD PRIMARY KEY (id);
+
+--ALTER TABLE
+--  friends
+--ALTER COLUMN
+--  id
+--    INT NOT NULL;
+
+--UPDATE friends
+--SET name = 'Jane Smith'
+--WHERE id = 1;
+
+--ALTER TABLE friends
+--ADD email VARCHAR(60);
+
+--UPDATE friends
+--set email = '@myself23'
+--WHERE id = 3;
+
+--DELETE FROM friends
+--WHERE id = 3;
