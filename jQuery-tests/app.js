c94418f6a150597d561308f655d5dfbccfaa381b
troumel <62116356+troumel@users.noreply.github.com>
2021-04-18 23:11:04 +0300

    Add files via upload

---
+++ jQuery-tests/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.blue-box').animate({
+        'margin-left': '100px',
+        'height': '150px'
+    });
+
+    // $(".blue-box").animate({left: '250px'});
+});
